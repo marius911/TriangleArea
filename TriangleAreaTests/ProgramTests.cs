9e515afeaceb04fe35d956ad454c7b19a6fac734
Marius Iulian <marius.ancutescu@outlook.com>
2020-03-05 18:35:18 +0200

    added more unit tests

--- TriangleAreaTests/ProgramTests.cs
+++ TriangleAreaTests/ProgramTests.cs
@@ -6,11 +6,30 @@ namespace TriangleArea.Tests
     public class ProgramTests
     {
         [Fact]
+        public void ComputeDistanceOnSamePoint()
+        {
+            Point first = new Point(0, 0);
+            Point second = new Point(0, 0);
+
+            Assert.True(Program.GetDistanceBetweenPoint(first, second) == 0d);
+        }
+
+        [Fact]
         public void TestDistance()
         {
-            Point first = new Point(0, 7);
-            Point second = new Point(0, 2);
+            // 3, 4, 5 are pitagoric numbers
+            Point first = new Point(3, 0);
+            Point second = new Point(0, 4);
+
             Assert.True(Program.GetDistanceBetweenPoint(first, second) == 5d);
+        }
+
+        [Fact]
+        public void ComputeSemiperimeter()
+        {
+        
+
+            Assert.True(Program.GetSemiPerimeter(2, 4, 4) == 5d);
         }
     }
 }
